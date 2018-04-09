848c61271ef3a77fa77ea1c4bafc128c4c45b6be
xiongsyao <xiongshengyao@hotmail.com>
2018-04-09 17:53:04 +0800

    [style]:修改缩进

--- webpack.config.js
+++ webpack.config.js
@@ -64,11 +64,11 @@ module.exports = {
   },
 
   plugins: [
-  	new webpack.optimize.UglifyJsPlugin({
-  		compress: {
-  			warnings: false
-  		}
-  	})
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
   ]
 
 };
